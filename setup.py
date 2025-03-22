1700d17d35c02e9e6a1f1645e979b3bebd11eca7
Jay prakash Giri <Jaygiri0111@gmail.com>
2025-03-23 00:34:08 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Jay prakash Giri',
+    author_email= 'jaygiri0111@gmail.com',
+    packages= find_packages(), #--> it will try to find _int_.py(constructor)
+    install_requires = []
+
+)
